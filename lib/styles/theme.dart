8bce84879b992c4f83aef3e9ec99b7e8b3bc9b4e
Karla Chable <Karla.Chable@itksquare.edu.mx>
2022-11-02 14:17:12 -0600

    theme lato

--- lib/styles/theme.dart
+++ lib/styles/theme.dart
@@ -3,6 +3,6 @@ import 'package:google_fonts/google_fonts.dart';
 
 class CustomTheme {
   var theme = ThemeData(
-    textTheme: GoogleFonts.kanitTextTheme(),
+    textTheme: GoogleFonts.latoTextTheme(),
   );
 }
